6b4ceb287a85b542def6186879554c7a5b77fcb0
Mohammad Mc <mohammad.chitsaz80@gmail.com>
2022-07-20 17:53:30 +0430

    I put some codes in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Hi Git!");
+
+const Hi = () => {
+  return "Hi";
+};
+
+export default Hi;
+
+console.log("Hello I'm Mohammad");
+console.log("Hello I'm Mohammad");
+console.log("Hello I'm Mohammad");
+console.log("Hello I'm Mohammad");
